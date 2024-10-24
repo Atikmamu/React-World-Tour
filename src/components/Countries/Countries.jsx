c29ce44c7499f60fbdb3500bd1cb1faac343a99f
Atikmamu <robinatik50@gmail.com>
2024-10-24 20:19:26 +0600

    all file added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,24 +1,70 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
+// array distructuring 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([])
+    const [visitedFlags, setVisitedFlags] = useState ([]);
 
-
+    //  useEffect fetch 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data));
     }, [])
 
+        //  event handler 
+    const handleVisitedCountry = country => {
+        console.log('add this to your visited country');
+        const newVisitedCountry = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountry);
+    }
+
+    const handleVisitedFlags = flag => {
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    }
+
+    // remove item from an array in a state 
+    // use filter to select all the elements except the one you want to remove 
+
+    
+
+
 
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }  
+            {/* visited country  map chalaisi*/}
+            <div>
+                <h5>Visited Countries: {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>
+                        {country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+              {/* visited flags ar jonno map chalaisi */}
+             <div className="flag-container">
+                  {
+                    visitedFlags.map((flag, index) => <img key={index} src={flag}></img>)
+                  }
+             </div>
+
+            {/* display countries map chalaisi */}
+          <div className="country-container">
+          {
+                countries.map(country => <Country 
+                    key={country.cca3}
+                    handleVisitedCountry = {handleVisitedCountry}
+                    handleVisitedFlags = {handleVisitedFlags}
+                    country={country}></Country>)
+            }
+          </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,31 @@
-import './Country.css'
+import { useState } from 'react';
+import './Country.css';
 
-const Country = ({country}) => {
-    const {name, flags} = country;
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
+    const {name, flags, area, capital, population, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }    
+    
+
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{color: visited ? 'purple' : 'black'}}>{name.common}</h3>
             <img src={flags.png} alt="" />
+            <h3>area: {area}</h3>
+            <h3>capital: {capital}</h3>
+            <h3>Population: {population}</h3>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            {visited ? 'I have visited this country.' : 'I want to visit'}
         </div>
     );
 };
